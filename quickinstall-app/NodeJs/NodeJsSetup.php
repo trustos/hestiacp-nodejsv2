facf5f404aec4db7c789ffb0f327ae874454ffe7
Yavor Tenev <tenevyavor@gmail.com>
2024-10-12 01:44:26 +0300

    Changed version on the plugin info.

--- quickinstall-app/NodeJs/NodeJsSetup.php
+++ quickinstall-app/NodeJs/NodeJsSetup.php
@@ -22,7 +22,7 @@ class NodeJsSetup extends BaseSetup
         "name" => "NodeJs",
         "group" => "node",
         "enabled" => true,
-        "version" => "1.0.0",
+        "version" => "2.0.0",
         "thumbnail" => "nodejs.png",
     ];
     protected $appname = "NodeJs";
