cddd92d830a20bc6170549ece9758cf02037afc6
Yavor Tenev <tenevyavor@gmail.com>
2024-10-17 09:59:01 +0300

    Added start script to be autofilled if exists.

--- quickinstall-app/NodeJs/env-vars-script.js
+++ quickinstall-app/NodeJs/env-vars-script.js
@@ -26,7 +26,13 @@ document.addEventListener("DOMContentLoaded", function () {
     envContainer.style.display = "none"; // Initially collapsed
 
     Object.keys(appData).forEach(function (key) {
-      if (key.toLowerCase() === "port") {
+      if (key === "start_script") {
+        const startScriptInput = form.querySelector(
+          '[name="webapp_' + key + '"]',
+        );
+        startScriptInput &&
+          startScriptInput.setAttribute("value", appData[key]);
+      } else if (key.toLowerCase() === "port") {
         const portInput = form.querySelector(
           '[name="webapp_' + key.toLowerCase() + '"]',
         );
